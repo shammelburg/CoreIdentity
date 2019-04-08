04a835fbaf3809314aa29b9539873a03efef49c5
Sander Hammelburg <sander.hammelburg@gmail.com>
2019-04-08 15:32:03 +0100

    add: for getting userId

---
+++ CoreIdentity.API/Helpers/UserHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CoreIdentity.API.Helpers
+{
+    public class UserHelper
+    {
+        public static string GetUserId(ClaimsPrincipal User)
+        {
+            return User.FindFirst("uid")?.Value;
+        }
+
+        public static string GetUserEmail(ClaimsPrincipal User)
+        {
+            return User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+        }
+    }
+}
